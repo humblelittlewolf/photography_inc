405d151347995e46134ac3aa8f8aa9f0b74b617c
Paul Whalen <paul.whalen@my.simpson.edu>
2016-11-03 11:39:07 -0500

    changed navigation to match php

--- public_html/navigation.php
+++ public_html/navigation.php
@@ -8,13 +8,13 @@
                 <a class="navbar-brand" href="index.html">Photography Inc</a>
             </div>
             <ul class="nav navbar-nav">
-              <li> <a href= "index.html"> Home </a> </li>
-              <li> <a href= "about_us.html"> About Us </a> </li>
-              <li> <a href= "camera_tutorials.html"> Camera Tutorials </a> </li>
-              <li> <a href= "hours.html"> Hours, Location, &amp; Directions </a> </li>
-              <li> <a href= "subject_ideas.html"> Subject and Location Ideas </a> </li>
-              <li> <a href= "sign_up.html"> Sign Up </a> </li>
-              <li> <a href= "log_in.html"> Log In </a> </li>
+              <li> <a href= "index.php"> Home </a> </li>
+              <li> <a href= "about_us.php"> About Us </a> </li>
+              <li> <a href= "camera_tutorials.php"> Camera Tutorials </a> </li>
+              <li> <a href= "hours.php"> Hours, Location, &amp; Directions </a> </li>
+              <li> <a href= "subject_ideas.php"> Subject and Location Ideas </a> </li>
+              <li> <a href= "sign_up.php"> Sign Up </a> </li>
+              <li> <a href= "log_in.php"> Log In </a> </li>
             </ul>
         </div>
     </nav>
