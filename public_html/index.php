29432d698c2f18338ecd99a16e3d7225c8cfae92
Paul Whalen <paul.whalen@my.simpson.edu>
2016-11-03 11:35:33 -0500

    php

--- public_html/index.php
+++ public_html/index.php
@@ -36,21 +36,7 @@
     </head>
     
     <body>
-<div style="overflow: auto; height: 45px;">
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid" style="width: 100%">
-                    <ul class="nav navbar-nav" style="overflow: hidden; height: 45px">
-                        <li> <a class="navbar-brand" href= "index.html"> Photography Inc </a> </li>
-                        <li> <a href= "about_us.html"> About Us </a> </li>
-                        <li> <a href= "camera_tutorials.html"> Camera Tutorials </a> </li>
-                        <li> <a href= "hours.html"> Hours, Location, &amp; Directions </a> </li>
-                        <li> <a href= "subject_ideas.html"> Subject and Location Ideas </a> </li>
-                        <li> <a href= "sign_up.html"> Sign Up </a> </li>
-                        <li> <a href= "log_in.html"> Log In </a> </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <?php include '../resources/navigation.php'; ?>
         
         <h1 class="below_menu_container" style="text-align:center;font-size: 72px;font-family:Gadugi, sans-serif; width: 635px; ">Photography Inc.</h1>
         
@@ -64,7 +50,7 @@
         </p>
     
     
-        <footer> &copy; Copyright 2016, Ashtyne Madsen &amp; Kyann Brown </footer>
+        <?php include '../resources/footer.php'; ?>
 
 
     </body>
---
+++ public_html/navigation.php
@@ -0,0 +1,21 @@
+<header>
+
+    <img class="image" src="img/MadsenBrown_header.jpg" alt="Madsen-Brown Header" width="100%" />
+
+  <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.html">Photography Inc</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li> <a href= "index.html"> Home </a> </li>
+              <li> <a href= "about_us.html"> About Us </a> </li>
+              <li> <a href= "camera_tutorials.html"> Camera Tutorials </a> </li>
+              <li> <a href= "hours.html"> Hours, Location, &amp; Directions </a> </li>
+              <li> <a href= "subject_ideas.html"> Subject and Location Ideas </a> </li>
+              <li> <a href= "sign_up.html"> Sign Up </a> </li>
+              <li> <a href= "log_in.html"> Log In </a> </li>
+            </ul>
+        </div>
+    </nav>
+    </header>
